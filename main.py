45dec5e0455a835bf5bf1e164c693134a3e0e471
Alexey <77196217+moblamobla@users.noreply.github.com>
2023-08-20 01:10:48 +0700

    Commit
    New design

--- main.py
+++ main.py
@@ -1,30 +1,55 @@
 import os
-from tkinter import *
-from tkinter import filedialog as fd
+import tkinter
+import customtkinter
+from customtkinter import filedialog as fd
+import time
+
+start = time.time()
+
 
 def selectDir():
-    return fd.askdirectory()+'/'
+    return fd.askdirectory() + '/'
+
 
 def startRename(directory):
-    directory1=directory.replace('/', '\\')
-    files=sorted([path for path in os.listdir(directory) if os.path.isfile(directory+path)])
-    i=0
+    directory1 = directory.replace('/', '\\')
+    files = sorted([path for path in os.listdir(directory) if os.path.isfile(directory + path)])
+    i = 0
+
+    start = time.time()
 
     while files:
-        file=files[0]
-        ext=file.split('.')[-1]
+        file = files[0]
+        ext = file.split('.')[-1]
         if not os.path.isfile(f'{directory}{i}.{ext}'):
             name = f'{i}.{ext}'
-            os.rename(directory1+file, directory1+name)
+            os.rename(directory1 + file, directory1 + name)
             del files[0]
-        i+=1
+        i += 1
+
+    end = time.time() - start
+
+    tkinter.messagebox.showinfo(title="Информация", message=f"Время выполнения: {end} сек.")
+
+    # labelSpeedDef = customtkinter.CTkLabel(app, text=f"Время выполнения: {end} сек.", fg_color="transparent")
+    # labelSpeedDef.grid(row=1, column=1)
 
 
-root = Tk()
-root.title("PyFileName")
-root.geometry('50x50')
+app = customtkinter.CTk()
+app.title("PyFileName")
+app.geometry('260x50')
+x = (app.winfo_screenwidth() - app.winfo_reqwidth()) / 2
+y = (app.winfo_screenheight() - app.winfo_reqheight()) / 2
+app.wm_geometry("+%d+%d" % (x, y))
+app.resizable(width=False, height=False)
 
-b1 = Button(text='Запустить', command=lambda: startRename(selectDir()))
-b1.grid(row=0,column=1)
+end = time.time() - start
 
-root.mainloop()
+labelSpeedLoad = customtkinter.CTkLabel(app, text=f"Время загрузки: {end} сек.", fg_color="transparent")
+labelSpeedLoad.grid(row=2, column=1)
+
+b1 = customtkinter.CTkButton(master=app, text='Запустить', command=lambda: startRename(selectDir()))
+b1.grid(row=0, column=1)
+
+
+app.mainloop()
